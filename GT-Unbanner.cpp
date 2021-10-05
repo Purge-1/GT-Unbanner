83d033d5fa2af28ca6dc80acbae6b14a9c4972a9
Purge-1 <80547719+Purge-1@users.noreply.github.com>
2021-10-05 04:37:03 -0700

    Add files via upload

---
+++ GT-Unbanner.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include "GT-Unbanner.h"
+
+using namespace std;
+
+int main()
+{
+	system("color 1b");
+	cout << "Unbanning you." << endl;
+	cout << "This project is not tested. Please don't ask me for any help with it." << endl;
+	macunban();
+	cout << "Mac Changed!" << endl;
+	ridunban();
+	cout << "Rid Changed!" << endl;
+	system("pause");
+	return 0;
+}
